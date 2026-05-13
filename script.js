f20b0d05b6b6ccbef16a0ed2bafa1d608d8a9478
saigreennursery <saigreennursery2@gmail.com>
2026-05-13 12:55:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,62 @@ function addToCart(){
 alert("Product Added To Cart");
 
 }
+let slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(() => {
+
+slides[index].classList.remove("active");
+
+index++;
+
+if(index >= slides.length){
+  index = 0;
+}
+
+slides[index].classList.add("active");
+
+},3000);
+
+
+
+function darkMode(){
+
+document.body.classList.toggle("dark");
+
+}
+
+
+
+document.querySelectorAll('#pay-btn').forEach(button => {
+
+button.onclick = function(){
+
+var options = {
+
+"key":"YOUR_RAZORPAY_KEY",
+
+"amount":"50000",
+
+"currency":"INR",
+
+"name":"Sai Green Nursery",
+
+"description":"Plant Order",
+
+handler:function(response){
+
+alert("Payment Successful");
+
+}
+
+};
+
+var rzp = new Razorpay(options);
+
+rzp.open();
+
+}
+
+});
